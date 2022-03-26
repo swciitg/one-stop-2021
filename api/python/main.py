772814a1cebaaba7c0868e53cec28206243a52df
Imperial-lord <absatyaprakash01@gmail.com>
2022-03-26 12:16:34 +0530

    :sparkles: Add image scraping with fast api

--- api/python/main.py
+++ api/python/main.py
@@ -1,19 +1,46 @@
-import requests
-from bs4 import BeautifulSoup
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import gunicorn
+from pydantic import BaseModel
+import numpy as np
+from image_scrap_bs4 import get_images_with_headers
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# pydantic models
 
 
-def get_images_with_headers(query):
-    params = {
-        "q": query,
-        "tbm": "isch",
-        "content-type": "image/png",
-    }
-
-    html = requests.get("https://www.google.com/search", params=params)
-    soup = BeautifulSoup(html.text, 'html.parser')
-
-    for img in soup.select("img"):
-        print(img["src"])
+class request_body_image_scrap(BaseModel):
+    query: str
 
 
-get_images_with_headers('pizza')
+@app.get("/")
+def welcome():
+    return {"ping": "Hello. This is the documentation for One Stop. Go to /docs to see the Swagger documentation"}
+
+
+@app.post("/get_scraped_images", status_code=200)
+def get_scraped_images(data: request_body_image_scrap):
+    '''
+    Fetches scraped images for a query from Google Images Search. Uses Beautiful Soup.
+    '''
+
+    images = get_images_with_headers(data.query)
+    print(data.query)
+    print(images)
+
+    if not images:
+        raise HTTPException(
+            status_code=400, detail="Images could not be fetched")
+
+    return {'images': images}
