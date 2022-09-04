590cd8e33a30e9b961898d676582c147341fb8ab
Kunalpal215 <kunalpal215@gmail.com>
2022-09-04 19:11:20 +0530

    fixed a bug

--- api/javascript/controllers/lostAndFoundControllers.js
+++ api/javascript/controllers/lostAndFoundControllers.js
@@ -17,11 +17,11 @@ exports.getImage = async (req, res) => {
   console.log("Get image par");
   const imagePath = path.resolve(
     __dirname +
-      "/../" +
-      "images_folder" +
-      "/" +
-      req.query.photo_id +
-      "-compressed.jpg"
+    "/../" +
+    "images_folder" +
+    "/" +
+    req.query.photo_id +
+    "-compressed.jpg"
   );
   console.log(imagePath);
   res.sendFile(imagePath);
@@ -31,26 +31,26 @@ exports.getCompressedImage = async (req, res) => {
   console.log("Get image par");
   const imagePath = path.resolve(
     __dirname +
-      "/../" +
-      "images_folder" +
-      "/" +
-      req.query.photo_id +
-      "-ultracompressed.jpg"
+    "/../" +
+    "images_folder" +
+    "/" +
+    req.query.photo_id +
+    "-ultracompressed.jpg"
   );
   console.log(imagePath);
   res.sendFile(imagePath);
 };
 
-exports.getLostPageDetails = async (req,res) => {
+exports.getLostPageDetails = async (req, res) => {
   let page = req.query.page;
-  const toSkip = (page-1)*5;
+  const toSkip = (page - 1) * 5;
   const docsCount = await LostModel.countDocuments();
-  if(toSkip>docsCount){
-      res.json({result : false, details : []});
-      return;
+  if (toSkip > docsCount) {
+    res.json({ result: false, details: [] });
+    return;
   }
-  const lostItems = await LostModel.find().sort({"date" : -1}).skip(toSkip).limit(5);
-  res.json({result: true,details : lostItems});
+  const lostItems = await LostModel.find().sort({ "date": -1 }).skip(toSkip).limit(5);
+  res.json({ result: true, details: lostItems });
 }
 
 exports.getLostDetails = async (req, res) => {
@@ -125,19 +125,19 @@ exports.postLostDetails = async (req, res) => {
         imageName;
       const newImagePath = path.resolve(
         __dirname +
-          "/../" +
-          "images_folder" +
-          "/" +
-          imageName +
-          "-compressed.jpg"
+        "/../" +
+        "images_folder" +
+        "/" +
+        imageName +
+        "-compressed.jpg"
       );
       const compressedImagePath = path.resolve(
         __dirname +
-          "/../" +
-          "images_folder" +
-          "/" +
-          imageName +
-          "-ultracompressed.jpg"
+        "/../" +
+        "images_folder" +
+        "/" +
+        imageName +
+        "-ultracompressed.jpg"
       );
       //const imageURL = "https://femefun.com/contents/videos_screenshots/50000/50719/preview.mp4.jpg";
       try {
@@ -208,16 +208,16 @@ exports.deleteLosts = async (req, res) => {
 
 // found details
 
-exports.getFoundPageDetails = async (req,res) => {
+exports.getFoundPageDetails = async (req, res) => {
   let page = req.query.page;
-  const toSkip = (page-1)*5;
+  const toSkip = (page - 1) * 5;
   const docsCount = await FoundModel.countDocuments();
-  if(toSkip>docsCount){
-      res.json({result : false, details : []});
-      return;
+  if (toSkip > docsCount) {
+    res.json({ result: false, details: [] });
+    return;
   }
-  const foundItems = await FoundModel.find().sort({"date" : -1}).skip(toSkip).limit(5);
-  res.json({result: true,details : foundItems});
+  const foundItems = await FoundModel.find().sort({ "date": -1 }).skip(toSkip).limit(5);
+  res.json({ result: true, details: foundItems });
 }
 
 exports.getfoundDetails = async (req, res) => {
@@ -241,18 +241,18 @@ exports.addfoundForm = async (req, res) => {
 exports.claimFoundItem = async (req, res) => {
   console.log("fjkdfgh");
   try {
-    
+
     const { id, claimerEmail, claimerName } = req.body;
     // console.log(req.body);
     let foundItem = await FoundModel.findById(id);
     console.log("fsdf");
     console.log(foundItem);
-    if (foundItem!=null && foundItem["claimed"]===true) {
+    if (foundItem != null && foundItem["claimed"] === true) {
       res.json({ saved: false, message: "This item already got claimed" });
       return;
     }
     await FoundModel
-      .findByIdAndUpdate(id,{
+      .findByIdAndUpdate(id, {
         claimed: true,
         claimerEmail: claimerEmail,
         claimerName: claimerName
@@ -303,19 +303,19 @@ exports.postfoundDetails = async (req, res) => {
         imageName;
       const newImagePath = path.resolve(
         __dirname +
-          "/../" +
-          "images_folder" +
-          "/" +
-          imageName +
-          "-compressed.jpg"
+        "/../" +
+        "images_folder" +
+        "/" +
+        imageName +
+        "-compressed.jpg"
       );
       const compressedImagePath = path.resolve(
         __dirname +
-          "/../" +
-          "images_folder" +
-          "/" +
-          imageName +
-          "-ultracompressed.jpg"
+        "/../" +
+        "images_folder" +
+        "/" +
+        imageName +
+        "-ultracompressed.jpg"
       );
       //const imageURL = "https://femefun.com/contents/videos_screenshots/50000/50719/preview.mp4.jpg";
       try {
