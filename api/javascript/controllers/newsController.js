cece2c3543ffc33a432b65f7f0ba6ffae4825a50
JKP-2001 <j.pandey@iitg.ac.in>
2022-11-02 02:28:20 +0530

    Added Routes For News

--- api/javascript/controllers/newsController.js
+++ api/javascript/controllers/newsController.js
@@ -1,4 +1,5 @@
 const NewsModel = require("../models/newsModel");
+const password = "ONE-STOP-IITG"
 exports.getNewsItems = async (req, res) => {
     try {
       const getevents = await NewsModel.find({}).sort({ dateCreated: 1 });
@@ -8,6 +9,20 @@ exports.getNewsItems = async (req, res) => {
       res.status(400).send(e);
     }
   }
+
+exports.createNewsItem = async (req,res)=>{
+  try {
+    if (req.body.password !== password) {
+      res.status(400).send("Invalid Password");
+    }
+    else{
+      const newsEvent = await NewsModel.create(req.body);
+      res.status(201).send(newsEvent);
+    } 
+  } catch (e) {
+    res.status(400).send(e);
+  }
+}
 
 exports.updateNewsItem = async (req, res) => {
     try {
