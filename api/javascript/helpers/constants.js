769ba49f587c049abcb634fd993beb9a76e58643
Kunalpal215 <kunalpal215@gmail.com>
2023-01-30 23:25:04 +0530

    added all clubs

--- api/javascript/helpers/constants.js
+++ api/javascript/helpers/constants.js
@@ -1,6 +1,5 @@
 const allIITGHostels = ["Brahmaputra","Kameng","Dihing","Barak","Kapili","Lohit","Manas","Married Scholars (Men)","Married Scholars (Women)","Siang","Subansiri","Umiam","Dhansiri","Disang (Men)","Disang (Women)"];
-const allIITGTechClubs =  ["Coding Club","CnA Club","E-Cell","Robotics Club"];
+const allIITGTechClubs =  ["Coding Club","CnA Club","E-Cell","Robotics Club","Aeromodelling Club","Automobile Club","Robotics Club","Electronics Club","IITG.AI","Gamedev and Esports","Equinox","Consulting and Analytics Club","Finance and Economics Club","Whitespace","Acumen","4i Labs"];
 
 exports.allIITGHostels = allIITGHostels;
-
 exports.allIITGTechClubs=allIITGTechClubs;
