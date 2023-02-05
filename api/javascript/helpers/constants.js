cf0748f4c3c7b436331da70e563c00766dcae307
Kunalpal215 <kunalpal215@gmail.com>
2023-02-05 12:06:51 +0530

    updated constants

--- api/javascript/helpers/constants.js
+++ api/javascript/helpers/constants.js
@@ -2,9 +2,19 @@ exports.allIITGHostels = ["Brahmaputra","Kameng","Dihing","Barak","Kapili","Lohi
 exports.allIITGTechClubs =  ["Coding Club","CnA Club","E-Cell","Robotics Club","Aeromodelling Club","Automobile Club","Electronics Club","IITG.AI","Gamedev and ESports","Equinox","FEC","Whitespace","Acumen","4i Labs"];
 
 exports.allIITGGymkhanaBoards = {
-    "SWC" : "k.pal@iitg.ac.in"
+    "Sports Board" : "h.nandigrama@iitg.ac.in",
+    "Students Web Committee (SWC)" : "k.pal@iitg.ac.in",
+    "Technical Board" : "r.vignesh@iitg.ac.in",
+    "Welfare Board" : "r.vignesh@iitg.ac.in",
+    "Student Alumni Interaction Linkage (SAIL)" : "h.nandigrama@iitg.ac.in",
+    "Vice President, Students Gymkhana" : "r.vignesh@iitg.ac.in"
 }
 
 exports.IITGAdminDepts = {
-    "Maintainence" : "kunalpal215@gmail.com"
+    "Maintainence" : "kunalpal215@gmail.com",
+    "Services" : "h.nandigrama@iitg.ac.in",
+    "Academic" : "r.vignesh@iitg.ac.in",
+    "Rights and Responsiblities" : "h.nandigrama@iitg.ac.in",
+    "RTI (Right to Information)" : "k.pal@iitg.ac.in",
+    "Medical Related Issues" : "r.vignesh@iitg.ac.in"
 }
