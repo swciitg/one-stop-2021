2544252e5f5c3fe47e14fd0008cfdd86e17eaa04
Ankit D <ankit.99d@gmail.com>
2023-04-11 23:13:21 +0530

    admin panel ui changes

--- api/javascript/admin_panel/admin-config.js
+++ api/javascript/admin_panel/admin-config.js
@@ -8,11 +8,18 @@ const messMenuResouce = require("./resources/messMenu.resource");
 const userResource = require("./resources/user.resource");
 const ferryTimingResource = require("./resources/ferryTiming.resource");
 const busStopResource = require("./resources/busStop.resource");
+const ui = require("./ui/loader");
+const { componentLoader, Components } = ui;
+const styleAssets = require("./ui/style-assets");
+
 AdminJs.registerAdapter(AdminJsMongoose);
-
-
 const adminjs = new AdminJs({
     resources: [messMenuResouce, userResource, ferryTimingResource, busStopResource],
+    assets: {
+        styles: styleAssets,
+    },
+    componentLoader,
+    dashboard: { component: Components.Dashboard },
     rootPath: ADMINPANELROOT,
     loginPath: ADMINPANELROOT + "/login",
     logoutPath: ADMINPANELROOT + "/logout",
--- api/javascript/admin_panel/utils.js
+++ api/javascript/admin_panel/utils.js
@@ -1,5 +1,5 @@
 module.exports = function verifyRoles(user, roles) {
-    console.log(user);
+    // console.log(user);
     let filtered = user.roles.filter((role) => roles.includes(role));
     return filtered.length > 0 ? true : false;
 };
