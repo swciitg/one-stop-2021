b3628e42f2b6452b2e15d185a28f19dd0161db22
Kunalpal216 <kunalpal215@gmail.com>
2023-06-29 17:43:32 +0530

    fixed notif issue

--- api/javascript/middlewares/error.handler.js
+++ api/javascript/middlewares/error.handler.js
@@ -4,6 +4,7 @@ exports.errorHandler = (err,req,res,next) => {
     console.log("here in error");
     console.log(err);
     if(err instanceof CustomError){
+        console.log("here 1");
         res.status(err.statusCode).json({"message" : err.message,"error" : err.name});
     }
     else{
