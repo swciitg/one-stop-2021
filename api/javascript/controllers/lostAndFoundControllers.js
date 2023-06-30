9b5750c91853b5f258622e5043eaaaec66b8ed0d
Kunalpal216 <kunalpal215@gmail.com>
2023-06-30 09:57:34 +0530

    changed notif for lost & found

--- api/javascript/controllers/lostAndFoundControllers.js
+++ api/javascript/controllers/lostAndFoundControllers.js
@@ -90,7 +90,7 @@ async function sendLostNotif(req,res,title){
     category: "LOST",
     model: "",
     header: title,
-    body: "An item has been lost"
+    body: `${req.body.username}'s belonging got lost. \n ${req.body.description.slice(0,20)}...`
   }
   await sendToAll(req,res);
 }
@@ -292,7 +292,7 @@ async function sendFoundNotif(req,res,title){
     category: "FOUND",
     model: "",
     header: title,
-    body: "An item was found"
+    body: `${req.body.username} found some item. \n ${req.body.description.slice(0,20)}`
   }
   await sendToAll(req,res);
 }
