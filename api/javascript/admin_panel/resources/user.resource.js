380cad985ee85cd939e7335e1e7c5b6636d5b361
Kunalpal216 <kunalpal2115@gmail.com>
2023-08-06 22:40:45 +0530

    updated user resource

--- api/javascript/admin_panel/resources/user.resource.js
+++ api/javascript/admin_panel/resources/user.resource.js
@@ -7,10 +7,10 @@ let allowedRoles = [roles.SUPERADMIN,roles.ONESTOPUSER];
 module.exports = {
     resource: user,
     options: {
-        listProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
-        filterProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
-        editProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
-        showProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
+        listProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
+        filterProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
+        editProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
+        showProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin"],
         actions: {
             list: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
             new: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
