1c561f640e4286bf54be4d04db9ee1f0ac32811f
Kunalpal216 <kunalpal2115@gmail.com>
2023-11-18 09:29:52 +0530

    fixed bug

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Security-Key"
     );
     next();
 });
