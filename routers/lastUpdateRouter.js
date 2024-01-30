9b9c715d2749d5ef8a91c491c85f36dc02eb2beb
swc <web_swc@iitg.ac.in>
2024-01-30 15:29:24 +0000

    Revert "added time-table update route"
    This reverts commit 190daf9de13d4afa16827675f37b9ea5f43e1c4f.

--- routers/lastUpdateRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const lastUpdateRouter = express.Router();
-const Controller = require("../controllers/lastUpdateController.js");
-const {verifyUserRequest} = require('../middlewares/user.auth');
-lastUpdateRouter.post("/lastupdate/timetable",verifyUserRequest, Controller.updatetimetableInLastUpdateDocument);
-
-module.exports =  lastUpdateRouter;
