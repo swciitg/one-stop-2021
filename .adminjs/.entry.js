49d3a3f9147e6e491da29ecbb65237d4de6d92c2
Arpit Sureka <surekaarpit@gmail.com>
2024-02-05 20:25:16 +0530

    changes

--- .adminjs/.entry.js
+++ .adminjs/.entry.js
@@ -1,3 +1,7 @@
 AdminJS.UserComponents = {}
 import Dashboard from '../admin_panel/ui/pages/dashboard'
 AdminJS.UserComponents.Dashboard = Dashboard
+import UploadImageEdit from '../admin_panel/ui/components/upload-image.edit'
+AdminJS.UserComponents.UploadImageEdit = UploadImageEdit
+import UploadImageList from '../admin_panel/ui/components/upload-image.list'
+AdminJS.UserComponents.UploadImageList = UploadImageList
