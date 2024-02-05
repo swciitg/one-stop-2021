5e97154b12afe88517149b077873f7f40b3629ab
Arpit Sureka <surekaarpit@gmail.com>
2024-02-05 20:31:52 +0530

    changes

--- admin_panel/resources/homePage.resource.js
+++ admin_panel/resources/homePage.resource.js
@@ -1,12 +1,15 @@
 const roles = require("../roles");
 const homePage = require("../../models/homePage");
 const uploadFeature = require('@admin-bro/upload')
+import { ComponentLoader } from "adminjs";
+const componentLoader = new ComponentLoader()
 
 let allowedRoles = [roles.SUPERADMIN, roles.LOST];
 
 module.exports = {
         resource: homePage,
         features: [uploadFeature({
+          componentLoader,
           provider: { local: { bucket: 'images_folder' } },
           properties: {
             key: 'fileUrl',
