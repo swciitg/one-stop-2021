ca5bef46897101c5426b46f42e58e68141b263de
Arpit Sureka <hardikroongta8@gmail.com>
2024-03-31 13:50:02 +0530

    added try catch to user.auth.js

--- middlewares/user.auth.js
+++ middlewares/user.auth.js
@@ -8,50 +8,58 @@ const { GuestAccessError } = require("../errors/guest.access.error");
 const { UserBlockedError } = require("../errors/user.blocked.error");
 
 exports.verifyUserRequest = async (req,res,next) => {
-    if(req.originalUrl.split('/').includes('public')) next();
-    console.log(req.originalUrl);
-    if(req.headers.authorization === undefined){
-        next(new RequestValidationError("Access token not passed"));
-    }
-    let accessToken = req.headers.authorization.split(' ').slice(-1)[0];
-    if(!accessToken) {
-        next(new RequestValidationError("Access token not passed"));
-    }
-    let decoded;
-    jwt.verify(accessToken, accessjwtsecret,(err,dec) => {
-        if(err){
-            next(new AccessTokenError(err.message));
+    try{
+        if(req.originalUrl.split('/').includes('public')) next();
+        console.log(req.originalUrl);
+        if(req.headers.authorization === undefined){
+            next(new RequestValidationError("Access token not passed"));
         }
-        decoded=dec;
-    });
-    console.log(decoded);
-    let onestopUser = await onestopUserModel.findById(decoded.userid);
-    if (onestopUser !== undefined && !onestopUser.blocked) {
+        let accessToken = req.headers.authorization.split(' ').slice(-1)[0];
+        if(!accessToken) {
+            next(new RequestValidationError("Access token not passed"));
+        }
+        let decoded;
+        jwt.verify(accessToken, accessjwtsecret,(err,dec) => {
+            if(err){
+                next(new AccessTokenError(err.message));
+            }
+            decoded=dec;
+        });
         console.log(decoded);
-        req.userid = decoded.userid;
-        req.body.email=onestopUser.outlookEmail;
-        console.log(req.userid);
-        console.log("Token Verified");
-        next();
+        let onestopUser = await onestopUserModel.findById(decoded.userid);
+        if (onestopUser !== undefined && !onestopUser.blocked) {
+            console.log(decoded);
+            req.userid = decoded.userid;
+            req.body.email=onestopUser.outlookEmail;
+            console.log(req.userid);
+            console.log("Token Verified");
+            next();
+        }
+        else if(onestopUser !== undefined && onestopUser.blocked){
+            next(new UserBlockedError("user has been blocked due to spamming"));
+        }
+        else next(new RequestValidationError("invalid user id found"));
+    }catch(err){
+        next(err);
     }
-    else if(onestopUser !== undefined && onestopUser.blocked){
-        next(new UserBlockedError("user has been blocked due to spamming"));
-    }
-    else next(new RequestValidationError("invalid user id found"));
 }
 
 exports.restrictIfGuest = async (req,res,next) => {
-    // since, verify user request will be checked before this so, token would always be present
-    let accessToken = req.headers.authorization.split(' ').slice(-1)[0];
-    let decoded;
-    jwt.verify(accessToken, accessjwtsecret,(err,dec) => {
-        if(err){
-            next(new AccessTokenError(err.message));
+    try {
+        // since, verify user request will be checked before this so, token would always be present
+        let accessToken = req.headers.authorization.split(' ').slice(-1)[0];
+        let decoded;
+        jwt.verify(accessToken, accessjwtsecret,(err,dec) => {
+            if(err){
+                next(new AccessTokenError(err.message));
+            }
+            decoded=dec;
+        });
+        if(decoded.userid === await getGuestUserID()){
+            next(new GuestAccessError("Guest Access not allowed"));
         }
-        decoded=dec;
-    });
-    if(decoded.userid === await getGuestUserID()){
-        next(new GuestAccessError("Guest Access not allowed"));
+        else next();
+    } catch (err) {
+        next(err);
     }
-    else next();
 }
