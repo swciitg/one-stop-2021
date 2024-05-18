b20fa35d385e0cba925af339148bd99ddeabc00c
Hardik Roongta <hardikroongta8@gmail.com>
2024-05-18 12:47:02 +0530

    admin panel changes for homeScreenCards

--- admin_panel/resources/homepage.resource.js
+++ admin_panel/resources/homepage.resource.js
@@ -5,26 +5,26 @@ const verifyRoles = require("../utils");
 let allowedRoles = [roles.SUPERADMIN]; // Only super admin allowed to change homepage image
 
 module.exports = {
-  resource: homePage,
-  options: {
-    listProperties: ["path", "clickableImageRedirectUrl"],
-    filterProperties: ["path", "clickableImageRedirectUrl"],
-    editProperties: ["path", "clickableImageRedirectUrl", "quickLinks"],
-    showProperties: ["path", "clickableImageRedirectUrl", "quickLinks"],
-    actions: {
-        list: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
-        new: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
-        filter: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
-        edit: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
-        delete: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) }
-    },
-    properties : {
-      quickLinks : {
-        description : "In Icon number add the number of the icon from https://fonts.google.com/icons - then convert code point to Decimal",
-      },
-      path : {
-        description : "Dont chnage this field, it is used to store the path of the image uploaded for the home page. To change the image, upload a new image and then delete the old one. If the path wont exist it will show the map image.",
-      },
+    resource: homePage,
+    options: {
+        listProperties: ["cardsDataList"],
+        filterProperties: ["cardsDataList"],
+        editProperties: ["cardsDataList", "quickLinks"],
+        showProperties: ["cardsDataList", "quickLinks"],
+        actions: {
+            list: {isAccessible: ({currentAdmin}) => verifyRoles(currentAdmin, allowedRoles)},
+            new: {isAccessible: ({currentAdmin}) => verifyRoles(currentAdmin, allowedRoles)},
+            filter: {isAccessible: ({currentAdmin}) => verifyRoles(currentAdmin, allowedRoles)},
+            edit: {isAccessible: ({currentAdmin}) => verifyRoles(currentAdmin, allowedRoles)},
+            delete: {isAccessible: ({currentAdmin}) => verifyRoles(currentAdmin, allowedRoles)}
+        },
+        properties: {
+            quickLinks: {
+                description: "In Icon number add the number of the icon from https://fonts.google.com/icons - then convert code point to Decimal",
+            },
+            path: {
+                description: "Dont change this field, it is used to store the path of the image uploaded for the home page. To change the image, upload a new image and then delete the old one. If the path wont exist it will show the map image.",
+            },
+        }
     }
-  }
 };
