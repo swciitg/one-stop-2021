f212b11d2b8a8e17a3f8cca4517938d23cf13cf7
Hardik Roongta <hardikroongta8@gmail.com>
2024-07-19 10:52:24 +0530

    Update constants.js

--- helpers/constants.js
+++ helpers/constants.js
@@ -10,11 +10,11 @@ exports.guestUserRollNo = '2110000000';
 exports.adminRoles = ["super", "board", "mmc", "hmc", "club"]
 
 exports.allIITGHostelsGC = ["Brahmaputra", "Kameng", "Dihing", "Barak", "Kapili", "Lohit", "Manas", "Married Scholars (Men)", "Married Scholars (Women)", "Siang", "Subansiri", "Umiam", "Dhansiri", "Disang (Men)", "Disang (Women)"];
-// exports.allIITGHostels = ["Brahmaputra","Kameng","Dihing","Barak","Kapili","Lohit","Manas","Married Scholars","Siang","Subansiri","Umiam","Dhansiri","Disang"];
+exports.allIITGHostels = ["Brahmaputra","Kameng","Dihing","Barak","Kapili","Lohit","Manas","Married Scholars","Siang","Subansiri","Umiam","Dhansiri","Disang"];
 exports.allIITGWomenHostels = ["Married Scholars (Women)", "Subansiri", "Dhansiri", "Disang (Women)"];
 exports.allIITGTechClubs = ["Coding Club", "CnA Club", "E-Cell", "Robotics Club", "Aeromodelling Club", "Automobile Club", "Electronics Club", "IITG.AI", "Gamedev and ESports", "Equinox", "FEC", "Whitespace", "Acumen", "4i Labs"];
 exports.allIITGWelfareClubs = ["Academic Initiatives Club", "Rights and Responsibilities Club", "Red Ribbon Club", "Youth Empowerment Club", "Saathi", "Social Service Club", "Substance Abuse Awareness Club"];
-exports.allIITGHostels = ["LOHIT", "BRAHMAPUTRA", "DISANG", "KAMENG", "BARAK", "MANAS", "DIHING", "UMIAM", "SIANG", "KAPILI", "DHANSIRI", "SUBHANSIRI", "MSH", "GAURANG", "DIBANG"];
+//exports.allIITGHostels = ["LOHIT", "BRAHMAPUTRA", "DISANG", "KAMENG", "BARAK", "MANAS", "DIHING", "UMIAM", "SIANG", "KAPILI", "DHANSIRI", "SUBHANSIRI", "MSH", "GAURANG", "DIBANG"];
 exports.allIITGGymkhanaBoards = {
     "Sports Board": "sportsec@iitg.ac.in",
     "Students Web Committee (SWC)": "swc@iitg.ac.in",
