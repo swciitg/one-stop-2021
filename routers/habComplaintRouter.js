60d880ae9d27c4aa9614322b0f540b680e894c35
skrj_ <shubhkarjha533@gmail.com>
2024-09-29 00:13:01 +0530

    dev pr

---
+++ routers/habComplaintRouter.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+const upload = require("../helpers/multer_single");
+const { submitHabComplaint } = require("../controllers/habComplaintController");
+const { restrictIfGuest, verifyUserRequest } = require("../middlewares/user.auth");
+const habComplaintRouter = express.Router();
+
+habComplaintRouter.post("/hab-complaint/file-upload",verifyUserRequest,upload.single("file"),(req,res) => {
+    res.json({"sucess" : true,"filename" : req.body.filename});
+});
+
+habComplaintRouter.post("/hab-complaint/submit-request",verifyUserRequest,restrictIfGuest,submitHabComplaint);
+
+module.exports = habComplaintRouter;
