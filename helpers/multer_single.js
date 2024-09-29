8ab9a2b11d661e7136096ce8d61a3824f527ed9c
AamirRaza01 <itsaamirrazaofficial@gmail.com>
2024-09-30 03:50:37 +0530

    Minor Fixes

--- helpers/multer_single.js
+++ helpers/multer_single.js
@@ -1,6 +1,15 @@
 // const multer = require("multer");
 // const fs = require("fs");
 // const uuid= require("uuid");
+
+// const path = require("path");
+
+// const uploadDir = path.join(__dirname, "/../files_folder/upsp_files/");
+
+// if(!fs.existsSync(uploadDir)){
+//     fs.mkdirSync(uploadDir, {recursive: true});
+// }
+
 // const fileStorageEngine = multer.diskStorage({
 //     destination: (req,file,cb) => {
 //         cb(null,__dirname + "/../files_folder/upsp_files/");
@@ -27,41 +36,27 @@
 
 const multer = require("multer");
 const fs = require("fs");
-const uuid = require("uuid");
-const path = require("path");
-
-// Define the destination directory
-const uploadDir = path.join(__dirname, "../files_folder/upsp_files");
-
-// Create the directory if it doesn't exist
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
+const uuid= require("uuid");
 const fileStorageEngine = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
+    destination: (req,file,cb) => {
+        cb(null,__dirname + "/../files_folder/upsp_files/");
     },
-    filename: (req, file, cb) => {
-        console.log("file ye hai ")
-        console.log(file);
-        let fileSaveName;
-        let parts = file.originalname.split(".");
-        let fileExtension = "." + parts[parts.length - 1];
-        parts.pop();
-
-        // Check if the file already exists
-        if (fs.existsSync(path.join(uploadDir, file.originalname))) {
-            fileSaveName = parts.join('.') + uuid.v4() + fileExtension;
-        } else {
-            fileSaveName = file.originalname;
-        }
-
-        req.body.filename = fileSaveName;
-        cb(null, fileSaveName);
+    filename: (req,file,cb) => {
+      console.log(file);
+      let fileSaveName;
+      let parts = file.originalname.split(".");
+      let fileExtension = "." + parts[parts.length-1];
+      parts.pop();
+      if (fs.existsSync(__dirname + "/../files_folder/upsp_files/" + file.originalname)) {
+        fileSaveName = parts.join('.')+uuid.v4()+fileExtension;
+      }else{
+        fileSaveName = file.originalname;
+      }
+      req.body.filename = fileSaveName;
+      cb(null, fileSaveName);
     }
 });
 
-const upload = multer({ storage: fileStorageEngine });
+const upload = multer({storage: fileStorageEngine});
 
-module.exports = upload;
+module.exports = upload
--- routers/upspRouter.js
+++ routers/upspRouter.js
@@ -4,10 +4,13 @@ const upload = require("../helpers/multer_single");
 const { restrictIfGuest, verifyUserRequest } = require("../middlewares/user.auth");
 const upspRouter = express.Router();
 
-upspRouter.post("/upsp/file-upload",upload.array("files",3), (req,res) => {
+upspRouter.post("/upsp/file-upload",verifyUserRequest,upload.array("file",5),(req,res) => {
+    console.log(req.body)
+    console.log(req.files)
+    console.log(req)
     res.json({"sucess" : true,"filename" : req.body.filename});
 });
 
-upspRouter.post("/upsp/submit-request", verifyUserRequest, restrictIfGuest, submitUpspForm);
+upspRouter.post("/upsp/submit-request",verifyUserRequest,restrictIfGuest,submitUpspForm);
 
 module.exports = upspRouter;
