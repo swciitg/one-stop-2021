8cb044afbd47b5beb70230156e027268f1d17849
dishantttt <k.dishant@iitg.ac.in>
2024-09-30 02:16:37 +0530

    revert upsp code

--- helpers/multer_single.js
+++ helpers/multer_single.js
@@ -1,15 +1,42 @@
+// const multer = require("multer");
+// const fs = require("fs");
+// const uuid= require("uuid");
+
+// const path = require("path");
+
+// const uploadDir = path.join(__dirname, "/../files_folder/upsp_files/");
+
+// if(!fs.existsSync(uploadDir)){
+//     fs.mkdirSync(uploadDir, {recursive: true});
+// }
+
+// const fileStorageEngine = multer.diskStorage({
+//     destination: (req,file,cb) => {
+//         cb(null,__dirname + "/../files_folder/upsp_files/");
+//     },
+//     filename: (req,file,cb) => {
+//       console.log(file);
+//       let fileSaveName;
+//       let parts = file.originalname.split(".");
+//       let fileExtension = "." + parts[parts.length-1];
+//       parts.pop();
+//       if (fs.existsSync(__dirname + "/../files_folder/upsp_files/" + file.originalname)) {
+//         fileSaveName = parts.join('.')+uuid.v4()+fileExtension;
+//       }else{
+//         fileSaveName = file.originalname;
+//       }
+//       req.body.filename = fileSaveName;
+//       cb(null, fileSaveName);
+//     }
+// });
+
+// const upload = multer({storage: fileStorageEngine});
+
+// module.exports = upload
+
 const multer = require("multer");
 const fs = require("fs");
 const uuid= require("uuid");
-
-const path = require("path");
-
-const uploadDir = path.join(__dirname, "/../files_folder/upsp_files/");
-
-if(!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir, {recursive: true});
-}
-
 const fileStorageEngine = multer.diskStorage({
     destination: (req,file,cb) => {
         cb(null,__dirname + "/../files_folder/upsp_files/");
