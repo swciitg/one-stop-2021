ba682cb084f4ad88520ae5ecc9370ac257db4817
skrj_ <shubhkarjha533@gmail.com>
2024-09-30 23:48:10 +0530

    minor change

--- controllers/habComplaintController.js
+++ controllers/habComplaintController.js
@@ -68,7 +68,7 @@ exports.submitHabComplaint = async (req,res) => {
                 </tr>
                 <tr>
                     <td style="padding: 20px;">
-                        <p>Dear Service Secretary of ${req.body.hostel} Hostel,</p>
+                        <p>Dear ${req.body.services==="Infra" ? "Maintenance" : "Service"} Secretary of ${req.body.hostel} Hostel,</p>
                         <p>This is an auto-generated email based on the response submitted by <strong>${req.body.name}</strong>.</p>
                     </td>
                 </tr>
@@ -101,7 +101,7 @@ exports.submitHabComplaint = async (req,res) => {
                 <!-- Footer -->
                 <tr>
                     <td style="padding: 20px; border-top: 1px solid #dddddd;">
-                        <p>Requesting the Hostel office to please follow up with the Service Secretary and General Secretary to ensure a response to the pending query if it remains unanswered.</p>
+                        <p>Requesting the Hostel office to please follow up with the ${req.body.services==="Infra" ? "Maintenance" : "Service"} Secretary and General Secretary to ensure a response to the pending query if it remains unanswered.</p>
                         <p style="margin-top: 40px; text-align: center;">
                             Thanks and Regards,<br>
                             <strong>Team SWC</strong><br>
