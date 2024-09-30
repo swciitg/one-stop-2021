d4e7e17b5cee1370b4cff787f7226ae4fefd2a58
Heet Patel <hiheet04@gmail.com>
2024-10-01 00:55:03 +0530

    changed email sending time back to 11:45 pm ist / 6:15 pm utc

--- helpers/cronJobs/opiEmails.js
+++ helpers/cronJobs/opiEmails.js
@@ -178,9 +178,10 @@ const scheduleOPIEmails = async () => {
     const startDate = opiStartDate ? moment.tz(opiStartDate, 'Asia/Kolkata').format('YYYY-MM-DD') : moment.tz('Asia/Kolkata').format('YYYY-MM-DD');
     const endDate = opiEndDate ? moment.tz(opiEndDate, 'Asia/Kolkata').format('YYYY-MM-DD') : moment.tz('Asia/Kolkata').add(5, 'days').format('YYYY-MM-DD');
 
-      cron.schedule("* * * * *", async () => {
+      // schedule cron job acc to 11:45 pm IST (6:10 pm UTC). Server is in UTC.
+      cron.schedule("15 18 * * *", async () => {
       // cron.schedule("* * * * *", async () => {
-      console.log("Running scheduled date processing at 11:40 PM IST...");
+      console.log("Running scheduled date processing at 11:45 PM IST...");
       await processDateList(startDate, endDate, recipients);
     }, {
       timezone: 'Asia/Kolkata'
