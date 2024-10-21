0f9cdff7dfc18c58e9aa35a1f2fe91f9f94112fd
dishantttt <k.dishant@iitg.ac.in>
2024-10-21 23:22:51 +0530

    minor bug fixes

--- admin_panel/resources/privateKey.resource.js
+++ admin_panel/resources/privateKey.resource.js
@@ -7,10 +7,6 @@ let allowedRoles = [roles.SUPERADMIN,roles.MESS];
 module.exports = {
     resource: privatekey,
     options: {
-        listProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin","subscribedMess"],
-        filterProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin","subscribedMess"],
-        editProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin","subscribedMess"],
-        showProperties: ["name", "outlookEmail","altEmail","rollNo","dob","gender","hostel","roomNo","blocked","notifPref","homeAddress","phoneNumber","emergencyPhoneNumber","linkedin","subscribedMess"],
         actions: {
             list: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
             new: { isAccessible: ({ currentAdmin }) => verifyRoles(currentAdmin, allowedRoles) },
