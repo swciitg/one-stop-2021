bbaa27098c5f9169a033bfcfd7a8e5125b4f78cc
rizzlibear <163667815+rizzlibear@users.noreply.github.com>
2024-11-02 23:10:31 +0530

    minor changes in messmenu form

---
+++ helpers/multer_hostelupload.js
@@ -0,0 +1,29 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      const uploadPath = path.join(__dirname, "/../files_folder/hostel_upload/");
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath); 
+      }
+      cb(null, uploadPath); 
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname); 
+    }
+  });
+  
+  const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+      if (path.extname(file.originalname) !== '.xlsx') {
+        return cb(new Error('Only .xlsx files are allowed!'));
+      }
+      cb(null, true);
+    }
+});
+
+module.exports = upload;
