783bf47f4a2a094bff560dbc0db2f9e4f8f27a27
Hardik Roongta <hardikroongta8@gmail.com>
2024-11-03 17:53:25 +0530

    minor chagne

--- controllers/hospitalFeedbackController.js
+++ controllers/hospitalFeedbackController.js
@@ -141,8 +141,8 @@ exports.doctorsFeedbackSubmit = async(req, res) => {
 
         let mailDetails = {
             from: process.env.UPSP_EMAIL,
-            // to : ["vp@iitg.ac.in", "gensec_welfare@iitg.ac.in", "mangal@iitg.ac.in" ] , //hospital-section mail to be added  
-            to : ["r.kareddy@iitg.ac.in", "m.raza@iitg.ac.in"] ,
+            to : ["vp@iitg.ac.in", "gensec_welfare@iitg.ac.in", "mangal@iitg.ac.in" ] , //hospital-section mail to be added  
+            // to : ["r.kareddy@iitg.ac.in", "m.raza@iitg.ac.in"] ,
             cc: patientEmail, // Send a copy to the patient
             subject: "New Doctor's Feedback",
             attachments: selectedAttachments,
@@ -233,8 +233,8 @@ exports.servicesFeedbackSubmit = async(req, res) => {
 
         let mailDetails = {
             from : process.env.UPSP_EMAIL ,
-            // to : ["vp@iitg.ac.in", "gensec_welfare@iitg.ac.in", "mangal@iitg.ac.in" ] , //hospital-section mail to be added
-            to : ["r.kareddy@iitg.ac.in", "m.raza@iitg.ac.in"] ,
+            to : ["vp@iitg.ac.in", "gensec_welfare@iitg.ac.in", "mangal@iitg.ac.in" ] , //hospital-section mail to be added
+            // to : ["r.kareddy@iitg.ac.in", "m.raza@iitg.ac.in"] ,
             cc : userEmail,
             subject : "New Hospital Feedback",
             attachments: selectedAttachments,
