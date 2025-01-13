3972d002761454c814cb41fb72ca672f75f2bea9
swciitg <swciitghy@gmail.com>
2025-01-13 19:37:42 +0530

    minor change

--- models/userNotifTokenModel.js
+++ models/userNotifTokenModel.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userNotifTokenSchema = new mongoose.Schema({
     userid: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     deviceToken: {
         type: String,
