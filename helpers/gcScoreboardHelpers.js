b519ebd8cbb40a12e45e7749141954ccc261f76b
Hardik Roongta <hardikroongta8@gmail.com>
2025-01-28 20:55:55 +0530

    Bug Fix for GC dev server

--- helpers/gcScoreboardHelpers.js
+++ helpers/gcScoreboardHelpers.js
@@ -27,15 +27,15 @@ exports.checkIfAdmin = async (email,competition) => {
 exports.checkIfBoardAdmin = async (email,competition) => {
     const gcScoreboardStore = await fetchGcScoreboardStore();
 
-console.log(gcScoreboardStore.spardha_board_admins);
-console.log(competition);
-console.log(email);
+    console.log(gcScoreboardStore.spardha_board_admins);
+    console.log(competition);
+    console.log(email);
 
     if((competition==='spardha' && gcScoreboardStore.spardha_board_admins.includes(email)) || 
     (competition==='manthan' && gcScoreboardStore.manthan_board_admins.includes(email)) || 
     (competition==='kriti' && gcScoreboardStore.kriti_board_admins.includes(email)) || 
     (competition==='sahyog' && gcScoreboardStore.sahyog_board_admins.includes(email))) return true;
-console.log("RETURNING FALSE");
+    console.log("RETURNING FALSE");
     return false;
 }
 
--- middlewares/gcChampionshipMiddlewares.js
+++ middlewares/gcChampionshipMiddlewares.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { checkIfAdmin, checkIfBoardAdmin } = require("../helpers/gcScoreboardHelpers");
 
 exports.gcRequestsMiddleware = async (req,res,next)=>{
+    let isProdEnv =  process.env.NODE_ENV === 'production';
+    let index = isProdEnv ? 5 : 6;
     try{
         const email = req.body.email;
         // console.log(req);
         // console.log(req["parsedUrl"]);
-        const competition = req.originalUrl.split('/')[5];
+        const competition = req.originalUrl.split('/')[index];
         console.log(email,competition);
         if(await checkIfAdmin(email,competition) || await checkIfBoardAdmin(email,competition)){ // check if he is any of admin/board_admin for the competition
             next();
