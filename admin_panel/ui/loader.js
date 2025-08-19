9f9b8a6c22a61fe2b54fdfc13e6a8a5068ddcbed
dishantttt <k.dishant@iitg.ac.in>
2025-08-19 21:13:16 +0530

    Minor fixxx

--- admin_panel/ui/loader.js
+++ admin_panel/ui/loader.js
@@ -1,32 +1,11 @@
-// import { ComponentLoader } from "adminjs";
-
-// const componentLoader = new ComponentLoader();
-
-// const Components = {
-//     Dashboard: componentLoader.add("Dashboard", "./pages/dashboard"),
-//     // other custom components
-// };
-
-// export { componentLoader, Components };
 import { ComponentLoader } from "adminjs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Resolve __dirname (since you're using ESM)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const componentLoader = new ComponentLoader();
 
-const isProd = process.env.NODE_ENV === "production";
-
 const Components = {
-  Dashboard: componentLoader.add(
-    "Dashboard",
-    isProd
-      ? path.resolve(__dirname, "../dist/admin_panel/ui/pages/dashboard.jsx")
-      : path.resolve(__dirname, "./pages/dashboard.jsx")
-  ),
+    Dashboard: componentLoader.add("Dashboard", "./pages/dashboard"),
+    // other custom components
 };
 
 export { componentLoader, Components };
+
