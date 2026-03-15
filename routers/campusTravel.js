644ae13652882520766999d4dfd206022438d013
Ayush Dixit <152638416+Ayush-Dix@users.noreply.github.com>
2026-03-15 21:21:29 +0530

    add request to join route

--- routers/campusTravel.js
+++ routers/campusTravel.js
@@ -12,5 +12,5 @@ campusTravelRouter.post("/campus-travel/chat", verifyUserRequest, restrictIfGues
 campusTravelRouter.delete("/campus-travel", verifyUserRequest, restrictIfGuest, campusTravelController.deleteTravelPost);
 campusTravelRouter.delete("/campus-travel/all", verifyUserRequest, restrictIfGuest, campusTravelController.deleteAllTravelPosts);
 campusTravelRouter.patch("/campus-travel/:postId/bookings/:bookingId/accept", verifyUserRequest, restrictIfGuest, campusTravelController.acceptBookingController);
-
+campusTravelRouter.post("/campus-travel/request-to-join", verifyUserRequest, restrictIfGuest, campusTravelController.requesttoJoin);
 export { campusTravelRouter };
